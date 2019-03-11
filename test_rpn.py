fecaebe772a452cdd9f6e82ebab3d4294047a9bc
nixl <nixl@umich.edu>
2019-03-10 20:28:35 -0400

    added tests for exception

--- test_rpn.py
+++ test_rpn.py
@@ -18,6 +18,17 @@ class TestBasics(unittest.TestCase):
         self.assertEqual(2, result)
     def test_exponential(self):
         result = rpn.calculate("2 2 ^")
+        self.assertEqual(4, result)
+    def value_error_fun(self):
+        result = rpn.calculate("1 + +")
+
+    def type_error_fun(self):
+        result = rpn.calculate("1 2 3 +")
+
+    def test_error(self):
+        self.assertRaises(Exception, self.value_error_fun)
+        self.assertRaises(Exception, self.type_error_fun)
+
 
 def main():
     test = TestBasics()
@@ -26,7 +37,7 @@ def main():
     test.test_multiply()
     test.test_divide()
     test.test_exponential()
-
+    test.test_error()
 
 
 if __name__ == '__main__':
